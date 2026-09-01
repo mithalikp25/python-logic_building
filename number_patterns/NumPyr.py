d5a9da819a65c474daceb7e4b11260c0e0660443
mithalikp25 <mithali.p@s.amity.edu>
2026-09-01 11:53:28 +0530

    Added number pattern Python programs

---
+++ number_patterns/NumPyr.py
@@ -0,0 +1,17 @@
+n = 5
+
+for i in range(1, n + 1):
+
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(1, 2 * i):
+        print(j, end="")
+
+    print()
+
+#     1
+#    123
+#   12345
+#  1234567
+# 123456789
